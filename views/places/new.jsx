ebd5feba827eea03e7918f74493f9027813f6ca1
danielFernandezJd <mdanif9728@gmail.com>
2024-05-16 14:50:05 -0700

    make change in index.jsx

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -12,14 +12,17 @@ function new_form() {
                         <label htmlFor="name">Place Name</label>
                         <input className="form-control" id="name" name="name" required />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" id="pic" name="pic" />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city" />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
